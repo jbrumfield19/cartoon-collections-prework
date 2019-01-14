8b9b80f4aa9bdac317d6e0108b6e8a84118a1ae5
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-14 17:21:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index{|name, i| puts "#{i+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  veggies.map{|n| n.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_planeteer_calls)
+if long_planeteer_calls.all?{|n| n.length >= 4}
+  return TRUE
+  elsif long_planeteer_calls.all?{|n| n.length < 4}
+  return FALSE
+ end
 end
 
 def find_the_cheese# code an argument here
